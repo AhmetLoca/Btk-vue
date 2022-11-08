9a964c5a4c722c852b9a3712634bef87bcf1c6c1
ahmet <locaahmet@gmail.com>
2022-11-08 15:19:30 +0300

    learn composables

--- 09-vue/src/components/Contents.vue
+++ 09-vue/src/components/Contents.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
     <div v-for="content in contents" :key="content.id">
-      {{ content.details }}
+      {{ content.title }}
+    </div>
+    <div v-if="err">
+      {{ err }}
     </div>
   </div>
 </template>
 
 <script>
+import getContentsFetch from "../composables/getContentsFetch";
+import { useRoute, useRouter } from "vue-router";
 export default {
   props: ["contents"],
-  setup(props) {
-    console.log(props.contents);
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
+/*
+Bir Route içerisindeki özelliklere params,meta,name,path gibi 
+özelliklere erişmek için Route.
+
+Router -> back, go , push, forward gibi özellikleri kullanabilemek için
+Router'u kullanacagiz.
+*/
+    console.log("Route:", route);
+    console.log("Router:", router);
+
+    const { contents, err, getContents } = getContentsFetch();
+    getContents();
+    return {
+      contents,
+      err,
+    };
   },
 };
 </script>
---
+++ 09-vue/src/composables/getContentsFetch.js
@@ -0,0 +1,25 @@
+import { ref } from "vue";
+
+const getContentsFetch = () => {
+  const contents = ref([]);
+  const err = ref(null);
+  const getContents = async () => {
+    try {
+      let data = await fetch("https://jsonplaceholder.typicode.com/posts");
+      contents.value = await data.json();
+
+      if (!data.ok) {
+        throw new Error("It is not connected the data");
+      }
+    } catch (error) {
+      err.value = error.message;
+    }
+  };
+  return {
+    contents,
+    err,
+    getContents,
+  };
+};
+
+export default getContentsFetch;
