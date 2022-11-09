57cd340a7f08be565e5d75ffe53b92c84058be1f
ahmet <locaahmet@gmail.com>
2022-11-09 15:14:56 +0300

    start an app

---
+++ 12-vue-the-learning-resources-app/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <li>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </li>
+</template>
+
+<script>
+// Components
+import LearningResource from "./components/learning-resources/LearningResource.vue";
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentatiton",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to google...",
+          link: "https://google.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
