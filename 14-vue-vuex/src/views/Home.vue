fbec2f9e8bc6f2d08fcaad20a352674dda8ea75c
ahmet <locaahmet@gmail.com>
2022-11-15 18:01:09 +0300

    intro vuex

---
+++ 14-vue-vuex/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">Home</div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "Home",
+  components: {},
+  setup() {
+    const todos = ref([
+      { id: 1, todo: "Ders calis" },
+      { id: 1, todo: "Film izle" },
+    ]);
+    return {
+      todos,
+    };
+  },
+};
+</script>
